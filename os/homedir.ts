08454bb0c0c88a9efeb9b6ce0ccd5b818714952d
pfconf <tablet.raga1@gmail.com>
2023-08-13 18:52:15 +0200

    improved homedir bug

--- os/homedir.ts
+++ os/homedir.ts
@@ -3,9 +3,9 @@ import { homedir } from "node:os";
 
 // https://github.com/oven-sh/bun/blob/main/src/bun.js/node/node_os.zig#L349
 // relies on HOME env variable
-// process.env is useless
+// but doesn't cover for unset HOME
 const { symbols: lib } = dlopen("/lib64/libc.so.6", {
-    putenv: {
+    unsetenv: {
         args: [FFIType.cstring],
         returns: FFIType.void
     }
@@ -14,6 +14,6 @@ const { symbols: lib } = dlopen("/lib64/libc.so.6", {
 const cstr = (s: string) => Buffer.from(s + '\0');
 
 // @ts-ignore broken types
-lib.putenv(cstr("HOME=Hey bun! Suck on deez nuts"));
+lib.unsetenv(cstr("HOME"));
 
 console.log(homedir());
